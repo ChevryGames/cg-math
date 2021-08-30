270bcd73eb18d90dca471eb155ea20c7a6680347
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-08-30 14:03:46 -0400

    Extracted static const

--- src/cg/math/dummy/dummy.d
+++ src/cg/math/dummy/dummy.d
@@ -5,13 +5,15 @@ module dummy;
 
 struct Dummy
 {
+    static const int MAX_VAR_INT = 3;
+
     int dummyVar1;
     string dummyVar2;
 
     this(int dummyVar1, string dummyVar2) nothrow
     in
     {
-        assert(dummyVar1 > 3);
+        assert(dummyVar1 > MAX_VAR_INT);
     }
     out
     {
