8b4703a642ce3963794024061267ec9b58292433
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-09-05 16:43:51 -0400

    Reformat

--- .github/workflows/config/build/get_d_compiler_version.sh
+++ .github/workflows/config/build/get_d_compiler_version.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 if [[ -v ${DC} ]]; then
-  >&2 echo "Error: D compiler \"${DC}\" (\"DC\" environment variable) not found."
+  echo >&2 "Error: D compiler \"${DC}\" (\"DC\" environment variable) not found."
   exit 1
 fi
 
@@ -11,7 +11,7 @@ dc_version_str="$(${DC} --version)"
 last_command_failed=$?
 
 if [[ ${last_command_failed} ]]; then
-  >&2 echo "Error: D compiler \"${DC}\" failed to supply its version."
+  echo >&2 "Error: D compiler \"${DC}\" failed to supply its version."
   exit 2
 fi
 
@@ -19,7 +19,7 @@ dc_version_line_str="$(echo "${dc_version_str}" | grep -i -E -o -m1 "D compiler 
 last_command_failed=$?
 
 if [[ ${last_command_failed} ]]; then
-  >&2 echo "Error: Failed to find a string line supplied by \"${DC}\" which contains the D compiler's version."
+  echo >&2 "Error: Failed to find a string line supplied by \"${DC}\" which contains the D compiler's version."
   exit 3
 fi
 
@@ -27,7 +27,7 @@ dc_version="$(echo "${dc_version_line_str}" | grep -i -E -o "[0-9]+\.[0-9]+\.[0-
 last_command_failed=$?
 
 if [[ ${last_command_failed} ]]; then
-  >&2 echo "Error: Failed to extract the D compiler's (\"${DC}\") version from a string line previously found which was supposed to contain it."
+  echo >&2 "Error: Failed to extract the D compiler's (\"${DC}\") version from a string line previously found which was supposed to contain it."
   exit 4
 fi
 
